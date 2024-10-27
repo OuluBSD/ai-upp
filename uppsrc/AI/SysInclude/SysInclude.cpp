c1b06402dc15759c0ebbf472577f0f693c7354cb
Seppo Pakonen <seppo.pakonen@gmail.com>
2024-10-28 00:55:07 +0200

    AI/SysInclude: win32 includes

--- uppsrc/AI/SysInclude/SysInclude.cpp
+++ uppsrc/AI/SysInclude/SysInclude.cpp
@@ -1,5 +1,12 @@
 #include "SysInclude.h"
+#undef main
 
+#if HAVE_WIN32 && (HAVE_GCC || HAVE_CLANG)
+int WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	return 0;
+}
+#else
 int main(int argc, char** argv){
 	return 0;
 }
+#endif

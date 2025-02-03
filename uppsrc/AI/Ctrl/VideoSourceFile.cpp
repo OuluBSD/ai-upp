7944ef81bd1d4862b33d6d823c83bcf78209a577
Seppo Pakonen <seppo.pakonen@gmail.com>
2025-02-03 19:19:59 +0200

    components

--- uppsrc/AI/Ctrl/VideoSourceFile.cpp
+++ uppsrc/AI/Ctrl/VideoSourceFile.cpp
@@ -3,11 +3,32 @@
 NAMESPACE_UPP
 
 VideoSourceFileCtrl::VideoSourceFileCtrl() {
+	CtrlLayout(*this);
+	
+	this->browse.WhenAction = [this]{
+		VideoSourceFile& comp = this->GetExt<VideoSourceFile>();
+		ValueMap map = comp.value;
+		String path = map("path");
+		String dir = path.Is() ? GetFileDirectory(path) : GetHomeDirectory();
+		FileSelNative sel;
+		sel.ActiveDir(dir);
+		if (path.Is())
+			sel.Set(GetFileName(path));
+		if (sel.ExecuteOpen("Select video file")) {
+			map = comp.value;
+			map.Set("path", sel.Get());
+			comp.value = map;
+			PostCallback(THISBACK(Data));
+		}
+	};
+	
 	
 }
 
 void VideoSourceFileCtrl::Data() {
-	
+	VideoSourceFile& comp = this->GetExt<VideoSourceFile>();
+	ValueMap map = comp.value;
+	this->path.SetData(map("path"));
 }
 
 void VideoSourceFileCtrl::ToolMenu(Bar& bar) {
@@ -22,6 +43,7 @@ INITIALIZER_COMPONENT_CTRL(VideoSourceFile, VideoSourceFileCtrl)
 
 
 VideoSourceFileRangeCtrl::VideoSourceFileRangeCtrl() {
+	CtrlLayout(*this);
 	
 }
 
@@ -41,6 +63,7 @@ INITIALIZER_COMPONENT_CTRL(VideoSourceFileRange, VideoSourceFileRangeCtrl)
 
 
 VideoSourceFileAudioCtrl::VideoSourceFileAudioCtrl() {
+	CtrlLayout(*this);
 	
 }
 

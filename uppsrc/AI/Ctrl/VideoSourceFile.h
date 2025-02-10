0b3855e6ec6f9e3f2e2b5f01f5247d0b5362f602
Seppo Pakonen <seppo.pakonen@gmail.com>
2025-02-10 19:31:52 +0200

    VideoSource components

--- uppsrc/AI/Ctrl/VideoSourceFile.h
+++ uppsrc/AI/Ctrl/VideoSourceFile.h
@@ -4,6 +4,16 @@
 NAMESPACE_UPP
 
 class VideoSourceFileCtrl : public WithVideoSourceFile<ComponentCtrl> {
+	String duration;
+	String container_str;
+	String duration_str;
+	String video_sample_str;
+	String video_codec_str;
+	String audio_sample_str;
+	String audio_codec_str;
+	String size_str;
+	String full_json;
+	Value last_value;
 	
 public:
 	typedef VideoSourceFileCtrl CLASSNAME;
@@ -11,22 +21,56 @@ public:
 	
 	void Data() override;
 	void ToolMenu(Bar& bar) override;
-	
+	void UpdateData();
 	
 };
 
 INITIALIZE(VideoSourceFileCtrl)
 
-class VideoSourceFileRangeCtrl : public WithVideoSourceFileRange<ComponentCtrl> {
-	
+class RangeCtrl : public Ctrl {
+	double range_begin = 0, range_end = 1;
+	double cursor = 0;
+	int GetTimePos(double t, int width) const;
+	double GetPos(int x, int width) const;
+	int is_selecting = 0;
+	int last_border = -1;
+public:
+	typedef RangeCtrl CLASSNAME;
+	RangeCtrl();
+	void Paint(Draw& d) override;
+	void LeftDown(Point p, dword keyflags) override;
+	void LeftUp(Point p, dword keyflags) override;
+	void MouseMove(Point p, dword keyflags) override;
+	void MouseLeave() override;
+	void Set(double cursor, double range_begin, double range_end);
+	void SetCursor(double d);
+	Event<double> WhenSetBegin, WhenSetEnd, WhenTime;
+};
+
+class VideoSourceFileRangeCtrl : public ComponentCtrl {
+	WithVideoSourceFileRange<Ctrl> header;
+	ImageViewerCtrl viewer;
+	RangeCtrl range;
+	Vector<Ptr<VideoSourceFile>> files;
+	Vector<String> file_paths;
+	String active_file;
+	double duration = 0;
+	double frame_rate = 1;
+	void GetImage(double time, Event<Image> cb);
+	void SetViewer(Image img);
+	void ChangeFile();
 public:
 	typedef VideoSourceFileRangeCtrl CLASSNAME;
 	VideoSourceFileRangeCtrl();
 	
 	void Data() override;
+	void DataFile();
+	bool UpdateFiles();
 	void ToolMenu(Bar& bar) override;
-	
-	
+	void Set(double t, int src);
+	void Play(bool range);
+	VideoSourceFile* GetFile();
+	String GetFilePath();
 };
 
 INITIALIZE(VideoSourceFileRangeCtrl)

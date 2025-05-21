a72ce39f45fa63e9c7d5cce021471f77bffa6b3f
Seppo Pakonen <seppo.pakonen@gmail.com>
2025-05-21 17:28:29 +0300

    AICore: stage compiler

--- uppsrc/AICore/StageCompiler.cpp
+++ uppsrc/AICore/StageCompiler.cpp
@@ -7,7 +7,33 @@ FarStageCompiler::FarStageCompiler() {
 }
 
 bool FarStageCompiler::Compile(Nod& stage) {
-	TODO
+	String txt = stage.value;
+	txt.Replace("\r", "");
+	
+	// Split ranges with separator '#define'
+	Vector<String> lines = Split(txt, "\n");
+	int line_i = -1;
+	VectorMap<String, Point> ranges;
+	Point cur;
+	String cur_header;
+	cur.x = 0;
+	for (String& line : lines) {
+		line_i++;
+		CParser p(line);
+		if (p.Char('#')) {
+			if (p.Id("define")) {
+				cur.y = line_i;
+				ranges.Add(cur_header, cur);
+				cur_header = TrimBoth(p.GetPtr());
+				cur.x = line_i+1;
+			}
+		}
+	}
+	cur.y = lines.GetCount()-1;
+	ranges.Add(cur_header, cur);
+	
+	DUMPM(ranges);
+	
 	return true;
 }
 

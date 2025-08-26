20f7e8ae3d5763c9db9df27090fb91d9b47573d8
Seppo Pakonen <seppo.pakonen@gmail.com>
2025-08-26 13:17:29 +0300

    Add initial LLDB debugger implementation

---
+++ uppsrc/ide/Debuggers/LLDBUtils.h
@@ -0,0 +1,42 @@
+#ifndef _ide_Debuggers_LLDBUtils_h_
+#define _ide_Debuggers_LLDBUtils_h_
+
+#include <Core/Core.h>
+
+namespace Upp {
+
+class ILLDBUtils {
+public:
+	virtual ~ILLDBUtils() = default;
+	
+	virtual String BreakRunning(int pid) = 0;
+};
+
+class LLDBUtilsFactory final {
+public:
+	One<ILLDBUtils> Create();
+};
+
+#if defined(PLATFORM_WIN32)
+
+class LLDBWindowsUtils final : public ILLDBUtils {
+public: /* ILLDBUtils */
+	virtual String BreakRunning(int pid) override;
+	
+private:
+	bool Is64BitIde() const;
+	bool Is64BitProcess(HANDLE handle) const;
+};
+
+#elif defined(PLATFORM_POSIX)
+
+class LLDBPosixUtils final : public ILLDBUtils {
+public: /* ILLDBUtils */
+	virtual String BreakRunning(int pid) override;
+};
+
+#endif
+
+}
+
+#endif

e436b4ef4d1837a3119bb6a4b21726cb6da5d109
Seppo Pakonen <seppo.pakonen@gmail.com>
2025-09-25 17:57:06 +0300

    Vfs rewrite progress

---
+++ uppsrc/Vfs/Core/Core.h
@@ -0,0 +1,10 @@
+// Consolidated Vfs/Core header (wrapper)
+#ifndef _Vfs_Core_Core_h_
+#define _Vfs_Core_Core_h_
+
+#include <Vfs/Core/VfsCore.h>
+
+// Placeholders for small common typedefs/enums can be added here as we extract.
+
+#endif
+

0c8147697712c5d3bda3168913bee38e58e5ab5d
Seppo Pakonen <seppo.pakonen@gmail.com>
2025-10-28 00:03:31 +0200

    script changes

--- script/build_upptst.sh
+++ script/build_upptst.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build:
-umk ./upptst,./uppsrc "${1}" ~/.config/u++/theide/CLANG.bm -bsH1 +GUI,USEMALLOC ".bin/${1}"
+umk ./upptst,./uppsrc "${1}" ~/.config/u++/theide/CLANG.bm -bsH1 +GUI,USEMALLOC "bin/${1}"
 
 # Run ide:
 echo "Executable compiled: .bin/${1}"

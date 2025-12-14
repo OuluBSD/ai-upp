75906680a3dcda1dff79bc85421bb306936e7a22
Seppo Pakonen <seppo.pakonen@gmail.com>
2025-12-14 17:20:33 +0200

    add uppdbg to build-console.sh

--- script/build-console.sh
+++ script/build-console.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build:
-umk upptst,examples,tutorial,reference,uppsrc "${1}" $HOME/.config/u++/theide/CLANG.bm -bsH1 +NOGUI,USEMALLOC,DEBUG_FULL "bin/${1}"
+umk upptst,uppdbg,examples,tutorial,reference,uppsrc "${1}" $HOME/.config/u++/theide/CLANG.bm -bsH1 +NOGUI,USEMALLOC,DEBUG_FULL "bin/${1}"
 
 # Run ide:
 echo "Executable compiled: bin/${1}"

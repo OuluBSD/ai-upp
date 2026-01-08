919f40122117e1deea161afeee7035491a7eb270
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-01-08 23:35:06 +0200

    upptst: HelloWorld2 for very simple upp compiling tests

---
+++ upptst/HelloWorldStd/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <HelloWorld2/HW2.h>
+
+int main(int argc, const char** argv) {
+	using namespace std;
+	cout << "Hello World" << endl;
+	cout << "Some fine function: " << SomeFineFunction(2) << endl;
+	return 0;
+}

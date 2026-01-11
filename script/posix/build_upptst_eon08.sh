3f78b96318a74bb10204cacec20ab59d79f08f83
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-01-11 17:53:57 +0200

    Eon08: related fix manual fix, no AI

--- script/posix/build_upptst_eon08.sh
+++ script/posix/build_upptst_eon08.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
-"$SCRIPT_DIR/build_upptst_eon_generic.sh" Eon08 "GUI,AI,SCREEN,HAL,OGL,FFMPEG,OPENCV,AUDIO,VIDEO,MEDIA" "$@"
+"$SCRIPT_DIR/build_upptst_eon_generic.sh" Eon08 "GUI,AI,SCREEN,HAL,FFMPEG,AUDIO,VIDEO,MEDIA" "$@"

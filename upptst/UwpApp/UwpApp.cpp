4b09e9c769aae36c6ce0d6a91e580070a02edcec
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-01-18 00:23:44 +0200

    Add UWP builder test package

---
+++ upptst/UwpApp/UwpApp.cpp
@@ -0,0 +1,8 @@
+#include "UwpApp.h"
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	Cout() << "UwpApp: hello from UWP builder test.\n";
+}
---
+++ upptst/UwpApp/UwpApp.h
@@ -0,0 +1,10 @@
+#ifndef _UwpApp_UwpApp_h_
+#define _UwpApp_UwpApp_h_
+
+#include <Core/Core.h>
+
+NAMESPACE_UPP
+
+END_UPP_NAMESPACE
+
+#endif

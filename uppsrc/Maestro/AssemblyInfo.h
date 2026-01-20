74f53d9720515ea7d866d4620603b513d120b9ce
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-01-20 17:21:32 +0200

    feat(Maestro): finalize AssemblyInfo struct
    - Add assembly_type and build_systems fields
    - Complete Task 02_RepoScanning/12_AssemblyInfo.md DONE

--- uppsrc/Maestro/AssemblyInfo.h
+++ uppsrc/Maestro/AssemblyInfo.h
@@ -4,15 +4,19 @@
 struct AssemblyInfo : Moveable<AssemblyInfo> {
 	String         name;
 	String         dir;
+	String         assembly_type;
 	Vector<String> packages;
 	Vector<String> package_dirs;
+	Vector<String> build_systems;
 
 	void Jsonize(JsonIO& jio) {
 		jio
 			("name", name)
 			("dir", dir)
+			("assembly_type", assembly_type)
 			("packages", packages)
 			("package_dirs", package_dirs)
+			("build_systems", build_systems)
 		;
 	}
 };

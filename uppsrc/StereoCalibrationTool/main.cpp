be491bf1d09be7603b690f783aeb2176938e720e
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-01-28 23:39:48 +0200

    StereoCalibrationTool: add cmdline capture tests

--- uppsrc/StereoCalibrationTool/main.cpp
+++ uppsrc/StereoCalibrationTool/main.cpp
@@ -1,9 +1,32 @@
 #include "StereoCalibrationTool.h"
+#include <cstring>
 
 using namespace Upp;
 
 GUI_APP_MAIN
 {
 	StereoCalibrationTool app;
+	const Vector<String>& args = CommandLine();
+	String usb_device;
+	int usb_timeout_ms = 0;
+	bool test_usb = false;
+	bool test_hmd = false;
+	int hmd_timeout_ms = 0;
+	for (const String& arg : args) {
+		if (arg == "--test-usb")
+			test_usb = true;
+		else if (arg == "--test-hmd")
+			test_hmd = true;
+		else if (arg.StartsWith("--usb-device="))
+			usb_device = arg.Mid(strlen("--usb-device="));
+		else if (arg.StartsWith("--usb-timeout-ms="))
+			usb_timeout_ms = atoi(arg.Mid(strlen("--usb-timeout-ms=")));
+		else if (arg.StartsWith("--hmd-timeout-ms="))
+			hmd_timeout_ms = atoi(arg.Mid(strlen("--hmd-timeout-ms=")));
+	}
+	if (test_usb)
+		app.EnableUsbTest(usb_device, usb_timeout_ms);
+	if (test_hmd)
+		app.EnableHmdTest(hmd_timeout_ms);
 	app.Run();
 }

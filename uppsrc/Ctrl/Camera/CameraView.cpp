bf658494b0b5837e620c0b88b899f8b26b0f3933
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-02-03 13:25:46 +0200

    Add WebcamRecorder overlay stats and V4L2 backend

--- uppsrc/Ctrl/Camera/CameraView.cpp
+++ uppsrc/Ctrl/Camera/CameraView.cpp
@@ -15,7 +15,10 @@ void CameraView::Paint(Draw& d) {
 	Size isz = img.GetSize();
 	Size dsz = sz;
 	Size fit = GetFitSize(isz, dsz);
-	d.DrawImage((dsz.cx - fit.cx) / 2, (dsz.cy - fit.cy) / 2, fit.cx, fit.cy, img);
+	Rect rc((dsz.cx - fit.cx) / 2, (dsz.cy - fit.cy) / 2, (dsz.cx + fit.cx) / 2, (dsz.cy + fit.cy) / 2);
+	d.DrawImage(rc.left, rc.top, fit.cx, fit.cy, img);
+	if (WhenOverlay)
+		WhenOverlay(d, rc, img);
 }
 
 END_UPP_NAMESPACE

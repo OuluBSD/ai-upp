57cd85307d44c0ae72fea00162b30a2a6e8e7b6d
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-02-03 12:34:32 +0200

    Define Draw/Video frame semantics

--- uppsrc/Draw/Camera/CameraTypes.h
+++ uppsrc/Draw/Camera/CameraTypes.h
@@ -2,9 +2,15 @@
 #define _Draw_Camera_CameraTypes_h_
 
 struct CameraFrame : VideoFrame {
+	bool is_bright = false;
+	int exposure = 0;
+	int64 serial = 0;
 };
 
 struct CameraStats : VideoStats {
+	int bright_frames = 0;
+	int dark_frames = 0;
+	int last_exposure = 0;
 };
 
 #endif

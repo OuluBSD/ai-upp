12f1acc036db44ba7201e73e17ea867a3983dc8d
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-02-03 23:37:22 +0200

    Move core media types to Draw/Media

---
+++ uppsrc/Draw/Media/Media.h
@@ -0,0 +1,16 @@
+#ifndef _Draw_Media_Media_h_
+#define _Draw_Media_Media_h_
+
+#include <Core/Core.h>
+
+NAMESPACE_UPP
+
+#include "Types.h"
+#include "Audio.h"
+#include "Video.h"
+#include "MediaStream.h"
+#include "FileIn.h"
+
+END_UPP_NAMESPACE
+
+#endif

23b14db48c816c86909df3af5d0cba37a8e01fff
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-02-03 11:58:57 +0200

    Add CameraDraw and CameraCtrl skeletons

---
+++ uppsrc/CameraCtrl/CameraCtrl.h
@@ -0,0 +1,13 @@
+#ifndef _CameraCtrl_CameraCtrl_h_
+#define _CameraCtrl_CameraCtrl_h_
+
+#include <CtrlLib/CtrlLib.h>
+#include <CameraDraw/CameraDraw.h>
+
+NAMESPACE_UPP
+
+#include "CameraView.h"
+
+END_UPP_NAMESPACE
+
+#endif

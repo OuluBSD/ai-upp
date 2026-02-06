e461a362cfcc0abca8b26523955b2ed922020e64
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-02-06 17:06:13 +0200

    Feature: Conversion Orchestrator integration with visual factory floor

--- uppsrc/MaestroHub/IntelligenceHub.cpp
+++ uppsrc/MaestroHub/IntelligenceHub.cpp
@@ -7,15 +7,18 @@ IntelligenceHub::IntelligenceHub() {
 	
 	tu_browser.Create();
 	log_analyzer.Create();
+	conversion.Create();
 	
 	tabs.Add(tu_browser->SizePos(), "Translation Unit Browser");
 	tabs.Add(log_analyzer->SizePos(), "Log & Trace Analyzer");
+	tabs.Add(conversion->SizePos(), "Conversion Factory");
 	tabs.Add(repo.SizePos(), "Repository Map");
 }
 
 void IntelligenceHub::Load(const String& maestro_root) {
 	if(tu_browser) tu_browser->Load(maestro_root);
 	if(log_analyzer) log_analyzer->Load(maestro_root);
+	if(conversion) conversion->Load(maestro_root);
 	
 	RepoScanner rs;
 	rs.Scan(maestro_root);

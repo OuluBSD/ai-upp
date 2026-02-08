45fa4dbbc6f2fd1309284883691f0f666696b5c6
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-02-09 01:22:50 +0200

    Aria/WebDriver: Fix profile selection, stealth injection timing, and hardened settings\n\n- WebDriver: Prioritize 'default-release' profile in profiles.ini discovery.\n- Navigator: Implement location.href trick and about:blank staging for early stealth injection in new tabs and navigation.\n- Firefox: Add browser.webdriver.privileged and prompt-connection preferences; restore essential stealth flags.\n- StealthJS: Refine navigator.webdriver masking to avoid detectability via enumerability.\n- Aria: Restore robust bot detection message parser for browserscan.net.

--- uppsrc/WebDriver/Firefox.cpp
+++ uppsrc/WebDriver/Firefox.cpp
@@ -33,8 +33,15 @@ void Firefox::ApplyStealthSettings(bool silence_audio) {
 	options.SetPreference("webgl.enable-webgl", true);
 	options.SetPreference("webgl.enable-debug-renderer-info", true);
 	
+	options.SetPreference("browser.webdriver.privileged", false);
+	options.SetPreference("devtools.debugger.prompt-connection", false);
+	
 	options.SetPreference("media.autoplay.default", 0);
 	options.SetPreference("media.autoplay.blocking_policy", 0);
+	
+	options.AddArgument("--disable-infobars");
+	options.AddArgument("--disable-extensions");
+	options.AddArgument("--no-sandbox");
 	
 	if (silence_audio)
 		options.SetPreference("media.volume_scale", "0.0");

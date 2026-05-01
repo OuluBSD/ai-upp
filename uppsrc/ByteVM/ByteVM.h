bdfbe7dd55cea88d9cb8c36a6917fecae41b5d0c
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-05-01 17:37:33 +0300

    refactor(ByteVM): complete reorganization into Python and EcmaScript sub-packages

--- uppsrc/ByteVM/ByteVM.h
+++ uppsrc/ByteVM/ByteVM.h
@@ -1,23 +1,6 @@
 #ifndef _ByteVM_ByteVM_h_
 #define _ByteVM_ByteVM_h_
 
-#include <Core/Core.h>
-#include <Core/TextParsing/TextParsing.h>
-
-#ifndef RTLOG
-#ifdef flagDEBUG_RT
-#define RTLOG(x) DLOG(x)
-#else
-#define RTLOG(x)
-#endif
-#endif
-
-#include "PyCode.h"
-#include "PyValue.h"
-#include "PyIR.h"
-#include "PyCompiler.h"
-#include "PyVM.h"
-#include "PolicyKit.h"
-#include "PyScheduler.h"
+#include <ByteVM/Python/Python.h>
 
 #endif
